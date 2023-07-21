15f07f8385b67c52d8b7e15c9e22f8f31aee148b
Shubham-S-Nimje <shubhamsnimje@gmail.com>
2023-07-22 02:23:49 +0530

    Debugging Techniques

--- routes.js
+++ routes.js
@@ -7,7 +7,7 @@ const requestHandler = (req, res) => {
   if (url === "/") {
     const displayMsg = fs.readFileSync("data.txt", "utf8");
     // console.log(displayMsg.toString())
-    
+
     res.setHeader("Content-type", "text/html");
     res.write("<html>");
     res.write("<head><Title>My First Page</Title></head>");
@@ -27,12 +27,13 @@ const requestHandler = (req, res) => {
 
     return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
+      const message = parsedBody.split("=")[0];
       //   fs.writeFileSync("data.txt", message, (err) => {
       //     res.statusCode = 302;
       //     res.setHeader("Location", "/");
       //     return res.end();
       //   });
+      
       fs.writeFileSync("data.txt", message);
       res.statusCode = 302;
       res.setHeader("Location", "/");
