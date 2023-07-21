b6c22bdddda16b59e2a35e8f5b6c417d279ef7a9
Shubham-S-Nimje <shubhamsnimje@gmail.com>
2023-07-21 18:10:08 +0530

    Request / Response task

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log("Shubham")
-    console.log(req)
+    // console.log(req.url, req.headers, req.method)
+    url = req.url
+    if(url === '/'){
+        res.setHeader('Content-type', 'text/html');
+        res.write('<html>');
+        res.write('<head><Title>My First Page</Title></head>');
+        res.write('<body><h1>Hello world</h1></body>');
+        res.write('</body>');
+        res.end();
+    }
+    else if(url === '/home'){
+        res.setHeader('Content-type', 'text/html');
+        res.write('<html>');
+        res.write('<head><Title>My First Page</Title></head>');
+        res.write('<body><h1>Welcome to Home Page</h1></body>');
+        res.write('</body>');
+        res.end();
+    }
+    else if(url === '/about'){
+        res.setHeader('Content-type', 'text/html');
+        res.write('<html>');
+        res.write('<head><Title>My First Page</Title></head>');
+        res.write('<body><h1>Welcome to About Page</h1></body>');
+        res.write('</body>');
+        res.end();
+    }
+    else if(url === '/node'){
+        res.setHeader('Content-type', 'text/html');
+        res.write('<html>');
+        res.write('<head><Title>My First Page</Title></head>');
+        res.write('<body><h1>Welcome to my Nodejs Project</h1></body>');
+        res.write('</body>');
+        res.end();
+    }
 })
 
 server.listen(4000)
