eab81d515c5e57069ba8b1cf5fdb76f8a56837ec
Shubham-S-Nimje <shubhamsnimje@gmail.com>
2023-07-22 00:45:11 +0530

    Writing and reading from a file

---
+++ redirecting.js
@@ -0,0 +1,44 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  url = req.url;
+  method = req.method;
+  
+  if (url === "/") {
+    const displayMsg = fs.readFileSync('data.txt','utf8')
+    // console.log(displayMsg.toString())
+
+    res.setHeader("Content-type", "text/html");
+    res.write("<html>");
+    res.write("<head><Title>My First Page</Title></head>");
+    res.write(`<body><h1>${displayMsg.toString()}</h1></body>`);
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="myMessage"><button type="submit">Submit</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  } 
+  if (url === "/message" && method === "POST") {
+    const body = [];
+
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+    //   fs.writeFileSync("data.txt", message, (err) => {
+    //     res.statusCode = 302;
+    //     res.setHeader("Location", "/");
+    //     return res.end();
+    //   });
+      fs.writeFileSync('data.txt', message);
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+      return res.end();
+    });
+  }
+});
+server.listen(4000);
